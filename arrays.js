0977876e360b196a79e25a6965887527f44add42
Tanner Williams <tannerw999@gmail.com>
2022-06-16 12:50:19 -0600

    finished demo

--- arrays.js
+++ arrays.js
@@ -2,3 +2,32 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = []
+
+backpack.push('Lightsaber')
+backpack.push('Robe', 'food')
+
+// console.log(backpack)
+
+let belt = backpack.shift()
+
+backpack.splice(1,0, 'coat')
+
+let bag = []
+
+bag.unshift('flint', 'blanket', 'toothbrush')
+
+backpack.push(bag.slice(0, 3))
+
+// console.log(backpack)
+// console.log(bag)
+// console.log(belt)
+
+for (let i = 0; i < bag.length; i++){
+    console.log(bag[i])
+}
+
+for (let i = 0; i < 3; i++){
+    console.log(backpack[i])
+}
